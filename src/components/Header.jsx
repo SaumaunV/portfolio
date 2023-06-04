5c9565a465df0ac25b57d81601e1eaec39691435
Saumaun <svahedipour1@gmail.com>
2023-06-03 22:50:19 -0500

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='menu'>
+        <div>Home</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
