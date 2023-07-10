caeab4dc20634422345f594416f5d41d74f56512
Saumaun <svahedipour1@gmail.com>
2023-07-09 19:52:34 -0500

    Update about page for mobile responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
+import { forwardRef } from 'react';
 import styles from './About.module.css';
 
-function About() {
+function About(_, ref) {
   return (
-    <div className={styles.container}>
+    <div ref={ref} className={styles.container}>
       <div className={styles.about}>
         <h1 className={styles.heading}>About</h1>
         <p>
@@ -35,7 +36,7 @@ function About() {
             <figcaption>JavaScript</figcaption>
           </figure>
           <figure>
-            <i class="devicon-typescript-plain colored"></i>
+            <i className="devicon-typescript-plain colored"></i>
             <figcaption>TypeScript</figcaption>
           </figure>
           <figure>
@@ -43,51 +44,51 @@ function About() {
             <figcaption>React</figcaption>
           </figure>
           <figure>
-            <i class="devicon-nextjs-original colored"></i>
+            <i className="devicon-nextjs-original colored"></i>
             <figcaption>Next.js</figcaption>
           </figure>
           <figure>
-            <i class="devicon-nodejs-plain colored"></i>
+            <i className="devicon-nodejs-plain colored"></i>
             <figcaption>Node.js</figcaption>
           </figure>
           <figure>
-            <i class="devicon-java-plain colored"></i>
+            <i className="devicon-java-plain colored"></i>
             <figcaption>Java</figcaption>
           </figure>
           <figure>
-            <i class="devicon-csharp-plain colored"></i>
+            <i className="devicon-csharp-plain colored"></i>
             <figcaption>C#</figcaption>
           </figure>
           <figure>
-            <i class="devicon-postgresql-plain colored"></i>
+            <i className="devicon-postgresql-plain colored"></i>
             <figcaption>PostgreSQL</figcaption>
           </figure>
           <figure>
-            <i class="devicon-mysql-plain colored"></i>
+            <i className="devicon-mysql-plain colored"></i>
             <figcaption>MySQL</figcaption>
           </figure>
           <figure>
-            <i class="devicon-express-original colored"></i>
+            <i className="devicon-express-original colored"></i>
             <figcaption>Express</figcaption>
           </figure>
           <figure>
-            <i class="devicon-html5-plain colored"></i>
+            <i className="devicon-html5-plain colored"></i>
             <figcaption>HTML</figcaption>
           </figure>
           <figure>
-            <i class="devicon-css3-plain colored"></i>
+            <i className="devicon-css3-plain colored"></i>
             <figcaption>CSS</figcaption>
           </figure>
           <figure>
-            <i class="devicon-tailwindcss-plain colored"></i>
+            <i className="devicon-tailwindcss-plain colored"></i>
             <figcaption>Tailwind CSS</figcaption>
           </figure>
           <figure>
-            <i class="devicon-firebase-plain colored"></i>
+            <i className="devicon-firebase-plain colored"></i>
             <figcaption>Firebase</figcaption>
           </figure>
           <figure>
-            <i class="devicon-graphql-plain colored"></i>
+            <i className="devicon-graphql-plain colored"></i>
             <figcaption>GraphQL</figcaption>
           </figure>
         </div>
@@ -96,4 +97,4 @@ function About() {
   );
 }
 
-export default About
+export default forwardRef(About)
