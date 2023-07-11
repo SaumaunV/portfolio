a8296e17df8e3b139c9729a9c2bc3ee71e2b15fc
Saumaun <svahedipour1@gmail.com>
2023-07-10 19:35:50 -0500

    Update styling for project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,23 +6,24 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 function Project({ image, name, description, stack, demoLink, githubLink }) {
   return (
     <div className={styles.container}>
-      <img src={image} alt={name} />
+      <img className={styles.img} src={image} alt={name} />
       <div className={styles.stack}>
         {stack.map((item) => (
-          <div className={styles.stackItem}>{item}</div>
+          <div key={item} className={styles.stackItem}>{item}</div>
         ))}
       </div>
       <h1 className={styles.heading}>{name}</h1>
-      <p>{description}</p>
+      <p className={styles.description}>{description}</p>
       <div className={styles.buttonContainer}>
         <a href={demoLink} target="_blank">
           <button className={styles.liveButton}>
-            <span>Live</span> <FontAwesomeIcon icon={faLink} />
+            <span>Live</span> 
+            <FontAwesomeIcon icon={faLink} />
           </button>
         </a>
         <a href={githubLink} target="_blank">
           <button className={styles.codeButton}>
-            <span>Source Code</span>
+            <span>Code</span>
             <i className={`devicon-github-original ${styles.githubIcon}`}></i>
           </button>
         </a>
